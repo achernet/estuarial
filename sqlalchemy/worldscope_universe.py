335cf3d393c8792e345b2cda701cf2eb41b77d59
Benjamin Zaitlen <quasiben@gmail.com>
2013-12-06 10:41:40 -0600

    adding worldscope query with universe index

--- sqlalchemy/worldscope_universe.py
+++ sqlalchemy/worldscope_universe.py
@@ -1,105 +1,119 @@
-import sqlalchemy
 import iopro 
 from iopro import pyodbc
-from sqlalchemy import Index, Table, Column, Integer, String, MetaData, ForeignKey, create_engine
-from sqlalchemy import DateTime, SmallInteger, Float, VARCHAR
-from sqlalchemy import DateTime as SmallDateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, mapper
-from sqlalchemy import and_
 import pandas as pd
+import numpy as np
 
-base = declarative_base()
 creds = {
-    "Uid": "bzaitlen",
-    "Pwd": "ZuLUpmxs",
-    "driver": "/usr/local/lib64/libsqlncli-11.0.so.1790.0",
-    "server": "107.21.201.126,2866"
+    "Uid": "qas_test",
+    "Pwd": "qasqasqas",
+    "driver": "/opt/microsoft/sqlncli/lib64/libsqlncli-11.0.so.1790.0",
+    "server": "10.143.14.62,1433"
 }
-conn = iopro.pyodbc.connect('Driver={%s};Server={%s};Database=qai;Uid={%s};Pwd={%s}'%(creds['driver'],creds['server'],creds['Uid'],creds['Pwd']))
 
 
-# db = create_engine('mssql+pyodbc://bzaitlen:ZuLUpmxs@107.21.201.126:2866/', encoding='latin1', echo=True)
+NI = 1751   # Net Income 
+CASH = 2001 # Cash
+TL = 3351   # Total Liabilities
+STD = 3051  # Short Term Debt
+LTD = 3251  # Long Term Debt
+TD = 3255   # Total Debt
+TA = 2999   # Total Assets
+
+
 def get_conn():
     conn = iopro.pyodbc.connect('Driver={%s};Server={%s};Database=qai;Uid={%s};Pwd={%s}'%(creds['driver'],creds['server'],creds['Uid'],creds['Pwd']))
     return conn
 
-sql = '''
-select m.seccode, d.year_, d.seq, d.date_, d.value_, f.date_ from wsndata d
-join secmapx m on m.ventype = 10 and m.vencode = d.code and rank = 1
-left outer join wsfye f on f.code = d.code and f.year_ = d.year_
-where m.seccode in (?, ?, ?) and item = ? and freq = ?'''
+def query(idx, entities, metrics):
 
-NI = '1751'
-FREQ = 'Q'
+    #default frequency to QUARTERLY
+    FREQ = 'Q'
 
-IBM = '36799'
-GOOG = '30902'
-AAPL = '6027'
+    sql = '''
+        SELECT  I.NAME
+            ,   S.TICKER
+            ,   S.CUSIP
+            ,   S.NAME
+            ,   N.DATE_
+            ,   D.CLOSE_
+            ,   N.SHARES
+            ,   M.SECCODE
+            ,   D.CLOSE_ * N.SHARES AS RELATIVE_MARKET_CAP
+        FROM         DBO.IDXSPCMP N
+            JOIN     DBO.IDXINFO I
+                ON  I.CODE = N.IDXCODE
+            JOIN     PRC.IDXSEC S
+                ON  S.CODE = N.SECCODE
+                AND S.VENDOR = 1 -- S&P
+            JOIN     DBO.SECMAP M
+                ON  M.SECCODE = S.PRCCODE
+                AND M.VENTYPE = 14 -- IDC Pricing
+                AND M.EXCHANGE = 1 -- US
+            JOIN     PRC.PRCDLY D
+                ON  D.CODE = M.VENCODE
+                AND D.DATE_ =(  SELECT  MAX(DATE_)
+                            FROM     PRC.PRCDLY
+                            WHERE   CODE = D.CODE
+                            AND DATE_ <= N.DATE_    )
+        WHERE       N.DATE_ = ?
+            AND I.TICKER = ?
+        ORDER BY    RELATIVE_MARKET_CAP DESC
+        '''
+    cursor = get_conn().cursor()
+    data = cursor.execute(sql,'2013-12-04','SPX_IDX').fetchdictarray()
+    df = pd.DataFrame.from_dict(data)
 
-params = [IBM,GOOG,AAPL,NI,FREQ]
+    #find proper seccodes
+    seccodes = [df[df['TICKER'] == sec]['SECCODE'].values[0] for sec in entities]
 
-cursor = get_conn().cursor()
-cursor.execute(sql,params)
-data = cursor.fetchdictarray()
-df = pd.DataFrame.from_dict(data)
+    #cursor doesn't like numpy.int32 convert to str
+    seccodes = [str(sec) for sec in seccodes]
 
-#select IBM
-df[df['seccode'] == int(IBM)].head(5)
-
-
-
-db = create_engine('mssql://', creator = get_conn, encoding='latin1', echo=True)
-Session = sessionmaker(bind=db)
-session=Session()
-
-metadata = MetaData()
-
-class WSNDATA(base):
-    __tablename__ = 'wsndata'
-    code = Column('Code', Integer, primary_key=True)
-    item = Column('Item', Integer, primary_key=True)
-    freq = Column('Freq', VARCHAR, primary_key=True)
-    year_ = Column('Year_', SmallInteger, primary_key=True)
-    seq_ = Column('Seq', Integer, primary_key=True)
-    date_ = Column('Date_', SmallDateTime)
-    value_ = Column('Value_', Float)
-
-class SECMAPX(base):
-    __tablename__ = 'secmapx'
-    seccode = Column('SecCode', Integer, primary_key=True)
-    ventype = Column('VenType', SmallInteger, primary_key=True)
-    vencode = Column('VenCode', Integer) 
-    rank = Column('Rank', SmallInteger, primary_key=True)
-    exchange = Column('Exchange', SmallInteger)
-    startdate = Column('StartDate', DateTime)
-    enddate = Column('EndDate', DateTime)
-
+    sql = '''
+          select item, m.seccode, d.year_, d.seq, d.date_, d.value_, f.date_ from wsndata d
+          join secmapx m on m.ventype = 10 and m.vencode = d.code and rank = 1
+          left outer join wsfye f on f.code = d.code and f.year_ = d.year_
+          where m.seccode in (%s) and item in (%s) and freq = ?
+          '''
     
-class WSFYE(base):
-    '''
-    World Scope Fiscal Year end Dates
-    Table returns fiscal year end dates for a given Worlscope entity ID
-    '''
+    secs = ', '.join('?' for sec in seccodes)
+    mets = ', '.join('?' for m in metrics)
+    sql = sql % (secs,mets)
 
-    __tablename__ = 'wsfye'
-    # code = Column('Code', Integer, ForeignKey('wsndata.code'), primary_key=True)
-    # year_ = Column('Year_', SmallInteger, ForeignKey('wsndata.year_'), primary_key=True)
-    code = Column('Code', Integer, primary_key=True)
-    year_ = Column('Year_', SmallInteger, primary_key=True)
-    date_ = Column('Date_', SmallDateTime)
+    print sql
+
+    params = seccodes+metrics+[FREQ]
+    cursor.execute(sql,params)
+    data = cursor.fetchdictarray()
+    df = pd.DataFrame.from_dict(data)
+
+    df['ticker'] = df['seccode']
+    df['ticker'] = df['ticker'].astype('str')
+    
+    for code, tick in zip(seccodes, entities):
+        df['ticker'].replace(code,tick,inplace=True)
+    
+    return df
 
 
-query = session.query(WSNDATA.year_, WSNDATA.seq_, WSNDATA.date_, WSNDATA.value_, WSFYE.date_, SECMAPX.seccode).\
-        join(SECMAPX, and_(SECMAPX.ventype == 10, SECMAPX.vencode == WSNDATA.code, SECMAPX.rank == 1)).\
-        outerjoin(WSFYE, and_(WSFYE.code == WSNDATA.code, WSFYE.year_ == WSNDATA.year_)).\
-        filter(SECMAPX.seccode.in_([IBM,AAPL,GOOG]), WSNDATA.item == 1751, WSNDATA.freq == 'Q')
+df = query('SP500', ['IBM','AAPL','MSFT'], [NI,CASH,TL,STD,TA])
 
-
-data_records = [rec.__dict__ for rec in query.all()]
-df = pd.DataFrame.from_records(data_records)
-df = df.drop(['_labels'],axis=1)
+print df.head(5)
 
 #select IBM
-df[df['seccode'] == int(IBM)].head(5)
+print df[df['ticker'] == 'IBM'].head(5)
+
+#select IBM and Net Income
+print df[(df['ticker'] == 'IBM') & (df['item'] == NI)].head(5)
+
+
+
+
+
+
+
+
+
+
+
 
