87e3275d42e7b598c9484cf39cd272f3b9e0c740
Benjamin Zaitlen <quasiben@gmail.com>
2014-03-24 09:21:11 -0500

    removing old test dir

--- tests/test_parsing_raw_sql.py
+++
@@ -1,7 +0,0 @@
-import estuarial
-import datetime as dt
-
-trmeta = estuarial.TRMETA()
-
-
-trmeta.to_fsql('tr_total_returns.sql','tr_total_returns.fsql')
--- tests/test_universe.py
+++
@@ -1,9 +0,0 @@
-import estuarial as fd
-
-spx = fd.UniverseBuilder.spx_idx('2013-12-04')
-dow = fd.UniverseBuilder.djx_idx('2013-12-04')
-
-dow.ohlc['2013-12-01':'2014-01-22']
-
-
-
