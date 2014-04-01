0f8dece70cdf18baa1e0db8a860125c27b2c2eee
Benjamin Zaitlen <quasiben@gmail.com>
2014-04-01 15:00:58 -0500

    working rkd queries added a test as well

--- estuarial/test/test_rkd.py
+++ estuarial/test/test_rkd.py
@@ -14,10 +14,22 @@ print metrics.rkd.find_metrics("Restaurants")
 meta = TRMETA()
 
 spx = UniverseBuilder.spx_idx('2013-12-04')
-seccodes = spx.data.seccode
+seccodes = spx.data.seccode.tolist()[:5]
+print seccodes[:5]
 
-rkd_secs = meta.to_rkdcode(seccodes)
+rkd_verse = meta.to_rkdcode(seccodes)
+print rkd_verse.head()
 
 
+from estuarial.query.trqad import TRQAD
+qad = TRQAD()
+dt_list = [dt.datetime(2000,1,1), dt.datetime(2014,1,1)]
 
+# Diluted EPS Excluding ExtraOrd Items
+# Total Debt
 
+metrics_list = ['SDBF','STLD']
+universe = rkd_verse.seccode.tolist()
+
+rkd_data = qad.fundamentals(universe=universe,metrics=metrics_list,dt_list=dt_list,DB="RKD")
+print rkd_data.head()
