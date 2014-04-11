0827f23e648cc17b3b855c54c174acf638d8815d
Ely M. Spears <spearsem@gmail.com>
2014-04-10 23:31:30 -0400

    Cleaned up the docstring generation for the auto-generated Yaml. Added sections for discovered indexes, constraints, and keys. Tested that it's working manually on a few tables. Still needs improvement of function documentation and review of foreign key stored procedure. There are over 4000 tables in sys.tables, so it will yield a large Yaml document and we should experiment with how to break it up into each distinct schema.

--- estuarial/drilldown/qad_schema.py
+++ estuarial/drilldown/qad_schema.py
@@ -14,11 +14,11 @@ def get_active_table_names():
     tables = rq.raw_query("SELECT * FROM sys.tables")
 
     # TODO: add processing to filter out deprecated tables and to
-    # affix the correct schema prefix (.dbo, .prc, etc.)
+    # affix the correct schema prefix (dbo, prc, etc.)
     return tables.name.values.tolist()
 
 
-def get_key_columns(sp_help_results):
+def get_key_columns(sp_help_results, sp_fkeys_results=None):
     """
     Build and execute a query that retrieves key columns from a particular
     table. Returns a list of regular keys and also a list of foreign keys.
@@ -26,14 +26,9 @@ def get_key_columns(sp_help_results):
     primary_key_defs = sp_help_results['constraints'].constraint_type.map(
         lambda x: x.startswith("PRIMARY KEY")
     )
-
-    foreign_key_defs = sp_help_results['constraints'].constraint_type.map(
-        lambda x: x.startswith("FOREIGN KEY")
-    )
  
     # Process primary keys if there were any.
     if primary_key_defs.any():
-
         primary_keys = (
             sp_help_results['constraints'][primary_key_defs]
             .constraint_keys
@@ -44,16 +39,9 @@ def get_key_columns(sp_help_results):
         primary_keys = []
 
     # Process foreign keys if there were any.
-    if foreign_key_defs.any():
-        foreign_keys = (
-            sp_help_results['constraints'][foreign_key_defs]
-            .constraint_keys
-            .values[0]
-            .split(", ")
-        )
-    else:
+    if sp_fkeys_results is None:
         foreign_keys = []
-
+    
     return primary_keys, foreign_keys
 
 
@@ -62,6 +50,8 @@ def get_column_names_and_types(sp_help_results):
     Build and execute a query that retrieves the available columns and their
     data types from a particular table.
     """
+    # Convert the `sp_help` result set for column information into a dict of
+    # the column names and their SQL types.
     column_metadata = sp_help_results['columns']
     column_type_dictionary = dict(
         zip(column_metadata.Column_name.values.tolist(), 
@@ -69,6 +59,29 @@ def get_column_names_and_types(sp_help_results):
     )
     
     return column_type_dictionary
+
+
+def execute_sp_fkeys(table_name):
+    """
+    Execute the `sp_fkeys` procedure to acquire foreign key information.
+    """
+    rq = RAW_QUERY()
+    tmp_conn = rq.aclient.raw_config.pyodbc.connect(
+        rq.aclient.raw_config.connstring
+    )
+
+    # Execute the foreign key stored procedure and convert the data.
+    tmp_curs = tmp_conn.cursor()
+    tmp_curs.execute("EXEC sp_fkeys @pktable_name='{}';".format(table_name))
+    raw_result_set = tmp_curs.fetchall()
+    column_names = [elem[0] for elem in tmp_curs.description]
+    tuple_data = [tuple(row) for row in raw_result_set]
+
+    # Return a DataFrame with None entries if no foreign keys.
+    return pandas.DataFrame(
+        tuple_data if tuple_data else [[None for elem in column_names]],
+        columns=column_names
+    )
 
 
 def execute_sp_help(table_name):
@@ -82,6 +95,7 @@ def execute_sp_help(table_name):
         rq.aclient.raw_config.connstring
     )
 
+    # Execute the `sp_help` stored procedure.
     tmp_curs = tmp_conn.cursor()
     tmp_curs.execute("EXEC sp_help '{}';".format(table_name))
 
@@ -97,6 +111,8 @@ def execute_sp_help(table_name):
         "owner", 
     ]
 
+    # For each retrieved result set, place the converted data into a pandas
+    # data frame.
     retrieved_data = {}
     has_next_set = True
     while has_next_set:
@@ -115,11 +131,61 @@ def execute_sp_help(table_name):
             has_next_set = tmp_curs.nextset()
 
         # pyODBC error happens on the last check of a result set when
-        # executing stored procedures.
+        # executing stored procedures. Break out of the while loop instead.
         except Exception as e:
             break
 
     return retrieved_data
+
+
+def make_index_documentation(sp_help_results):
+    """
+    Parse result set on SQL indexes into readable documentation.
+    """
+    doc_prefix = "            "
+    indexes = sp_help_results["indexes"]
+    index_doc = doc_prefix + "Indexes\n" + doc_prefix + "-------\n"
+    for i, row in indexes.iterrows():
+        index_doc += doc_prefix + "name: {}\n".format(row.ix["index_name"])
+        index_doc += doc_prefix + "keys: {}\n".format(row.ix["index_keys"])
+        index_doc += (
+            doc_prefix + 
+            "description: {}\n\n\n".format(row.ix["index_description"])
+        )
+    return index_doc
+
+def make_constraint_documentation(sp_help_results):
+    """
+    Parse result set on SQL constraint/keys into readable documentation.
+    """
+    doc_prefix = "            "
+    constraints = sp_help_results["constraints"]
+    constraint_doc = (
+        doc_prefix + 
+        "Constraints/Keys\n" + 
+        doc_prefix + 
+        "----------------\n"
+    )
+
+    for i, row in constraints.iterrows():
+        constraint_doc += (
+            doc_prefix + 
+            "name: {}\n".format(row.ix["constraint_name"])
+        )
+
+        constraint_doc += (
+            doc_prefix + 
+            "keys: {}\n".format(row.ix["constraint_keys"])
+        )
+
+        constraint_doc += (
+            doc_prefix + 
+            "description: {}\n\n\n".format(row.ix["constraint_type"])
+        )
+
+    # Leave off the last newline to reduce whitespace from following sections.
+    return constraint_doc[:-1]
+
 
 def make_function_documentation(sp_help_results, table_name):
     """
@@ -138,8 +204,7 @@ def make_function_documentation(sp_help_results, table_name):
 
     # Top-line documentation and columns with type info.
     doc_yaml_prefix = "            " # To get Yaml indendation correct.
-    doc_string = ""
-    doc_string += (
+    doc_string = (
         "Reflexive function to provide access to table '{}'.\n\n".format(
             table_name
         )
@@ -147,33 +212,21 @@ def make_function_documentation(sp_help_results, table_name):
 
     doc_string += doc_yaml_prefix 
     doc_string += "The table provides the following columns:\n\n"
+    doc_string += doc_yaml_prefix
+
     doc_string += (
         sp_help_results.get("columns", empty_columns_df)[columns_df_columns]
         .set_index("Column_name")
-        .to_string().replace("\n", "\n" + doc_yaml_prefix) + "\n\n"
+        .to_string().replace("\n", "\n" + doc_yaml_prefix) + "\n\n\n"
     ) 
 
     # Print-out of any indexes built on the table.
-    doc_string += doc_yaml_prefix 
-    doc_string += "The table provides the following indexes:\n\n"
+    index_docs = make_index_documentation(sp_help_results)
+    doc_string += index_docs
 
-    doc_string += doc_yaml_prefix 
-    doc_string += (
-        sp_help_results.get("indexes", empty_index_df)
-        .set_index("index_name").T
-        .to_string().replace("\n", "\n" + doc_yaml_prefix) + "\n\n"
-    )
-
-    # Print-out on any keys / foreign keys / constraints on the table.
-    doc_string += doc_yaml_prefix 
-    doc_string += "The table provides the following constraint/keys:\n\n"
-
-    doc_string += doc_yaml_prefix 
-    doc_string += (
-        sp_help_results.get("constraints", empty_constraint_df)
-        .set_index("constraint_name").T
-        .to_string().replace("\n", "\n" + doc_yaml_prefix) + "\n\n"
-    )
+    # Print-out of any keys / foreign keys / constraints on the table.
+    constraint_docs = make_constraint_documentation(sp_help_results)
+    doc_string += constraint_docs
 
     return doc_string
 
@@ -224,13 +277,11 @@ def make_function_yaml(name, table, doc, conditonals):
     {name}:
         doc: >
             {doc}
-
         conditionals:
             {conditionals}
-
-       query: >
-           SELECT *
-           FROM {table}
+        query: >
+            SELECT *
+            FROM {table}
 
 
     """
@@ -250,7 +301,7 @@ def make_composite_schema_yaml():
     """
     base_yaml = "QADSchema:\n"
     #table_names = get_active_table_names()
-    table_names = ['prc.PrcDly']
+    table_names = ['prc.PrcDly', 'dbo.DS2PrimQtRI', 'dbo.DS2ScdQtRI']
 
     for table in table_names:
         
