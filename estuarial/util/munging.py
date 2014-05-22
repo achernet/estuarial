29cacf051d02f29851809f68cd365a752c295892
Benjamin Zaitlen <quasiben@gmail.com>
2014-05-22 14:29:16 -0500

    fixing munging alignment 'fdate'->'date'

--- estuarial/util/munging.py
+++ estuarial/util/munging.py
@@ -5,12 +5,12 @@ from dateutil.relativedelta import relativedelta
 # different reporting dates.
 def worldscope_align(df):
     for count, row in df.iterrows():
-        if row['ddate'] is np.nan and row['fdate'] is not np.nan:
+        if row['ddate'] is np.nan and row['date'] is not np.nan:
             assert row['seq'] in range(1, 5)
             sh_months = 4 - row['seq']
-            row['ddate'] = row['fdate'] + relativedelta(months=-1*sh_months)
-            last_fisc_yr = row['fdate']
-        elif row['ddate'] is np.nan and row['fdate'] is np.nan:
+            row['ddate'] = row['date'] + relativedelta(months=-1*sh_months)
+            last_fisc_yr = row['date']
+        elif row['ddate'] is np.nan and row['date'] is np.nan:
             row['ddate'] = last_fisc_yr + relativedelta(months=3*row['seq'])
         df.loc[count, 'ddate'] = row['ddate']
     return df
